c73353665dd2ac3b799d09ec787c9df78abb52a8
vishvash <visvash@amazon.com>
2023-10-16 16:23:54 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,13 +1,4 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
-
-  control_object_ownership = true
-  object_ownership         = "ObjectWriter"
-
-  versioning = {
-    enabled = true
-  }
+module "s3-bucket" {
+  source  = "app.terraform.io/aws-terra-vish/s3-bucket/aws"
+  version = "2.8.0"
 }
